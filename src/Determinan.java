9efe5676f114cb3a4c356a58d947f8269a981085
blixa-rd <16521074@mahasiswa.itb.ac.id>
2022-10-02 15:06:09 +0700

    Determinan Reduksi Baris Fix

--- src/Determinan.java
+++ src/Determinan.java
@@ -23,4 +23,58 @@ public class Determinan {
         return det;
     }
 
+    public static double reductionRow(double[][] M, int N){
+        double temp[][] = new double[N][N];
+        double result = 0;
+        for (int row = 0; row < N; ++row){
+            for (int col = 0; col < N; ++col){
+                temp[row][col] = M[row][col];
+            }
+        }
+        int count = rowReduction(M, N);
+        double det = M[0][0];
+        for (int i = 1; i < N; i++){
+            det *= M[i][i];
+        }
+        for (int row = 0; row < N; ++row){
+            for (int col = 0; col < N; ++col){
+                M[row][col] = temp[row][col];
+            }
+        }
+        if (count % 2 == 0){
+            result = det;
+        }else{
+            result = -det;
+        }
+        return result;
+    }
+
+    public static int rowReduction(double[][] M, int N){
+        int i = 0, l = 0, idx;
+        double pereduksi;
+        int count = 0;
+        for (int j = 1; j < N; j++){
+            for (int h = j; h < N; h++){
+                idx = i+1;
+                while ((M[i][l] == 0) && (idx < N)){
+                    for (int k = 0; k < N; k++){
+                        double tmp = M[i][k];
+                        M[i][k] = M[idx][k];
+                        M[idx][k] = tmp;
+                    }
+                    count++;
+                    idx++;
+                }
+                if (idx == N){
+                    continue;}
+                pereduksi = M[h][l] / M[i][l];
+                for (int k = l; k < N; k++){
+                    M[h][k] -= M[i][k] * pereduksi;
+                }
+            }
+            i++;
+            l++;
+        }
+        return count;
+    }
 }
